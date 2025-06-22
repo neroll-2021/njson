46461a5a757825c5ad5d2e479c70c7242d0ba6c2
Neroll <1343809614way@gmail.com>
2025-06-22 15:33:25 +0800

    update target name

--- xmake.lua
+++ xmake.lua
@@ -3,7 +3,7 @@ add_rules("mode.debug", "mode.release")
 set_languages("c++20")
 set_warnings("all", "error")
 
-target("njson-plus")
+target("njson")
     set_kind("binary")
     add_includedirs("include")
     add_files("src/*.cpp")
